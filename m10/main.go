3a91788b92d9920c79f604a2f5bb119b45243966
liang <sky_blue_xuliang@163.com>
2022-04-24 21:58:49 +0800

    feat: m10，dashboard 增加 0.8 分位曲线，并 10s 自动刷新

--- m10/main.go
+++ m10/main.go
@@ -17,25 +17,31 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// 模块十作业（必交）
-// 1、为 HTTPServer 添加 0-2 秒的随机延时；
-// 2、为 HTTPServer 项目添加延时 Metric；
-// 3、将 HTTPServer 部署至测试集群，并完成 Prometheus 配置；
-// 4、从 Prometheus 界面中查询延时指标数据；
-// 5、（可选）创建一个 Grafana Dashboard 展现延时分配情况。
-// 提交地址： https://jinshuju.net/f/awEgbi
-// 截止日期：2022 年 4 月 24 日 23:59
+/*
+模块十作业（必交）
+1、为 HTTPServer 添加 0-2 秒的随机延时；
+2、为 HTTPServer 项目添加延时 Metric；
+3、将 HTTPServer 部署至测试集群，并完成 Prometheus 配置；
+4、从 Prometheus 界面中查询延时指标数据；
+5、（可选）创建一个 Grafana Dashboard 展现延时分配情况。
+提交地址： https://jinshuju.net/f/awEgbi
+截止日期：2022 年 4 月 24 日 23:59
+*/
 
 /*
-
 A:
+
+1/2、见代码
+	程序会自动请求自己的 hello 接口，以产生测试数据
+
+3、deployment 部署
+	deploy.yaml
 
 4、查询 99 分位耗时
 	histogram_quantile(0.99, sum(rate(httpserver_http_cost_bucket[5m])) by (le))
 
-5、配置
+5、dashboard 配置，0.5、0.8、0.9、0.95 分位耗时分布
 	dashboard/http-cost.json
-
 */
 
 var Log *logrus.Logger
@@ -68,9 +74,14 @@ func main() {
 
 	// 自动请求自己，用于产生测试数据
 	go func() {
-		time.Sleep(time.Second)
 		for {
-			http.Get("http://localhost/hello")
+			time.Sleep(time.Second)
+			go func() {
+				_, err := http.Get("http://localhost/hello")
+				if err != nil {
+					Log.Errorf("get hello err = %v", err)
+				}
+			}()
 		}
 	}()
 
@@ -106,6 +117,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 	delay := rand.Intn(2000)
 	time.Sleep(time.Duration(delay) * time.Millisecond)
+
 	_, err := io.WriteString(w, fmt.Sprintf("hello [%v]\n", delay))
 	if err != nil {
 		Log.Errorf("hello err = %v", err)
